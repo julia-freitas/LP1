02ef46e0fea67bdd4277655ca8140c6f40ddee78
Julia Freitas <julia__freitas2@hotmail.com>
2018-05-09 06:51:46 -0300

    Add files via upload

---
+++ temperatura/temps.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int temps[50];
+	int leitura = 0; // varia entre -100 e 100
+	int i;
+	int soma = 0;
+	float media = 0;
+
+	for(i = 0; i < 5; i++)
+	{
+		scanf("%d", &leitura);
+		
+		soma += leitura;
+		//printf("leitura: %d\n", leitura);
+		//printf("soma: %d\n", soma);
+		
+		temps[i] = leitura + '0'; // valor ascii do inteiro lido - conversao de inteiro para char
+	}	
+
+	media = soma/5.0;
+	printf("media: %.2f\n", media);
+
+	return 0;
+}
