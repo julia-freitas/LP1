2dbd0789ca4d85121c29f2b8adf4f38ff528412a
Julia Freitas <julia__freitas2@hotmail.com>
2018-06-11 12:59:02 -0300

    correcao ex-07-1.c

--- tipos-compostos/ex-07-1.c
+++ tipos-compostos/ex-07-1.c
@@ -1,1 +1,48 @@
+/*Crie uma struct qualquer relacionada ao seu projeto.
+	Seja criativo!
+Crie uma função preenche que recebe um ponteiro para o novo tipo criado 
+e leia os campos para a variável passada, ex.:
 
+struct T t;
+preenche(&t);
+printf(“A=%d, B=%d\n”, t.a, t.b);*/
+
+#include <stdio.h>
+
+
+struct Pessoa // struct que guarda informacoes de cada pessoa ao efetuar o cadastro
+{
+	int idade;
+	char curso[30];
+	char entrada[6];
+	char saida[6];
+
+};
+
+
+void preenche(struct Pessoa* nome);  //recebe o apontador para a struct para acessar e modificar seu conteudo
+
+int main()
+{
+	struct Pessoa julia;
+	preenche(&julia);
+
+	printf("idade = %d, curso = %s, entrada = %s, saida = %s\n", julia.idade, julia.curso, julia.entrada, julia.saida);
+	return 0;
+}
+
+void preenche(struct Pessoa* p)  //recebe o apontador para a struct para acessar e modificar seu conteudo
+{
+	printf("digite sua idade: ");
+	scanf("%d", &(*p).idade);
+	
+	printf("digite seu curso: ");
+	scanf("%s", (*p).curso);
+	
+	printf("digite seu horario padrao de entrada: ");
+	scanf("%s", (*p).entrada);
+	
+	printf("digite seu horario padrao de saida: ");
+	scanf("%s", (*p).saida);
+
+}
